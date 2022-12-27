7a288b409cf761b5b66298c48c53a05623af20ba
Aswin PM <111994281+aswin-official@users.noreply.github.com>
2022-12-27 22:47:46 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+# youtube = "aswin"
+
+# print(f"subscribe to {youtube}")
+# print("subscribe to" + youtube)
+# print("subscribe to {}".format(youtube))
+adj = input("Adjective: ")
+verb1 = input("enter the verb: ")
+verb2 = input("enter teh second verb: ")
+famous_guy = input("famous_person: ")
+
+madlib = (f"once i was {adj}  and a cat {verb1}  me with it's {verb2} and it was a cat of a {famous_guy}")
+print(madlib)
